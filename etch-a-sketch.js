c19972bfcc0a9c8a76a24d0aa9d76d70e2783b6e
JC8299 <justincheng.cheng@gmail.com>
2022-08-08 20:04:50 -0700

    Add button to clear and change size of grid
    Added minor changes to functions to make it easier to plan ahead for
    changing slowly to black. Also changed some colors to look a little more
    like the etch-a-sketch.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,7 +3,7 @@ function hovered(e) {
     this.style.backgroundColor = 'rgb(0,0,0)';
 }
 
-function createGrid(width, height) {
+function createGrid(side) {
     let squares = document.querySelectorAll('.square');
     squares.forEach(square => square.removeEventListener('mouseenter', hovered));
 
@@ -12,10 +12,10 @@ function createGrid(width, height) {
 
     let row;
     let square;
-    for (let i = 0; i < height; i++) {
+    for (let i = 0; i < side; i++) {
         row = document.createElement('div');
         row.setAttribute('class', 'squareRow');
-        for (let j = 0; j < width; j++) {
+        for (let j = 0; j < side; j++) {
             square = document.createElement('div');
             square.setAttribute('class', 'square');
             row.appendChild(square);
@@ -23,12 +23,31 @@ function createGrid(width, height) {
         grid.appendChild(row);
     }
 
-    document.body.appendChild(grid);
+    sketchContainer = document.querySelector('.sketchContainer');
+    sketchContainer.appendChild(grid);
     squares = document.querySelectorAll('.square');
     squares.forEach(square => square.addEventListener('mouseenter', hovered));
 
 }
 
+function newGrid() {
+    let newSide = Number(prompt("Enter side length that is at most 100"));
 
+    while (isNaN(newSide)) {
+        if (newSide !== null) {
+            break;
+        }
+        newSide = Number(prompt("Please enter a number that is at most 100"));
+    }
 
-createGrid(16, 16);
+    if (!isNaN(newSide) || newSide !== 0) {
+        if (newSide > 100) {
+            newSide = 100;
+            alert("100 is the max size. The side will be set to 100.");
+        }
+        document.querySelector('.sketchContainer').removeChild(document.querySelector('.squareGrid'));
+        createGrid(newSide);
+    }
+}
+
+createGrid(16);
